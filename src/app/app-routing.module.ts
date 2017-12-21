312dd45fba3ca4b58b7bf3131eeb42fe6dc245a8
Nicholas Murray <murray.nicholas@gmail.com>
2017-12-21 18:25:36 +0000

    Added first attempt at mobile slide menu and enabled ie polyfills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { IndexComponent } from './components/pages/index/index.component';
 import { CardsComponent } from './components/pages/cards/cards.component';
 import { FormfieldComponent } from './components/pages/formfield/formfield.component';
 
 const routes: Routes = [
+  { path: '', component: IndexComponent },
   { path: 'cards', component: CardsComponent },
   { path: 'formfield', component: FormfieldComponent }
 ];
